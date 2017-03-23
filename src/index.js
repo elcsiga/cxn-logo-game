b5958e292fea54eeb5bb7c154458f0a50e7ed22a
Gabor Csizmadia <gcsizmadia@chemaxon.com>
2017-03-23 17:10:42 +0100

    improve fullscreen handling

--- src/index.js
+++ src/index.js
@@ -22,9 +22,17 @@ function renderScene() {
     ReactDOM.render(<Scene width={window.innerWidth} height={window.innerHeight}/>, appElement);
 }
 
+function handleFullscreenChange() {
+    if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
+        window.screen.orientation.lock('portrait-primary').then(renderScene).catch(renderScene);
+    }
+}
+
 ReactDOM.render(<LandingPage/>, appElement);
 
-document.addEventListener('click', function () {
-    requestFullscreen();
-    window.screen.orientation.lock('portrait-primary').then(renderScene).catch(renderScene);
-});
+document.addEventListener('click', requestFullscreen);
+
+document.addEventListener('fullscreenchange', handleFullscreenChange);
+document.addEventListener('webkitfullscreenchange', handleFullscreenChange);
+document.addEventListener('mozfullscreenchange', handleFullscreenChange);
+document.addEventListener('onmsfullscreenchange', handleFullscreenChange);
