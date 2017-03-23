642d9a0b9007c13fb8cfbd3a83c4be03658260ea
Andras Kovacs <akovacs76@chemaxon.com>
2017-03-23 15:42:44 +0100

    fixing svg import

--- src/components/scene.js
+++ src/components/scene.js
@@ -3,29 +3,34 @@ import PathSeq from 'pathseg';
 import Matter from 'matter-js';
 import $ from 'jquery';
 
-class Scene extends React.Component {
+const logoSVG = require('../resources/logo_segments_3.svg');
 
+class Scene extends React.Component {
     componentDidMount() {
         // create an engine
         const engine = Matter.Engine.create();
+
+        const width = this.props.width;
+        const height = this.props.height;
 
         // create a renderer
         const render = Matter.Render.create({
             element: this.sceneElement,
             engine: engine,
             options: {
-                width: this.props.width,
-                height: this.props.height
+                width: width,
+                height: height,
+
+                wireframes: true,
+                showInternalEdges: true
             }
         });
 
 
         const self = this;
-        const svgFile = 'https://shared.chemaxon.com/users/akovacs76/svg/logo_segments.svg';
-        $.get(svgFile).done(function(data) {
-            var vertexSets = [];
+        $.get(logoSVG).done(function(data) {
 
-            console.log(window.decomp);
+            var vertexSets = [];
 
             $(data).find('path').each(function(i, path) {
                 vertexSets.push(Matter.Svg.pathToVertices(path, 30));
@@ -36,9 +41,7 @@ class Scene extends React.Component {
                 y: wertex.y*10
             })));
 
-
-            console.log('WSET',vertexSets);
-            const logo = Matter.Bodies.fromVertices(self.props.width/2, self.props.height/2, convertedVertexSets, {
+            const logo = Matter.Bodies.fromVertices(width/2, height/2, convertedVertexSets, {
                 isStatic: true,
                 render: {
                     fillStyle: '#2e2b44',
@@ -49,29 +52,18 @@ class Scene extends React.Component {
 
             Matter.World.add(engine.world, logo);
 
-            /*var bodyOptions = {
-                frictionAir: 0,
-                friction: 0.0001,
-                restitution: 0.6
-            };
 
-            World.add(world, Composites.stack(80, 100, 20, 20, 10, 10, function(x, y) {
-                if (Query.point([terrain], { x: x, y: y }).length === 0) {
-                    return Bodies.polygon(x, y, 5, 12, bodyOptions);
-                }
-            }));*/
+            // create two boxes and a ground
+            const balls = [];
+
+            for (let i = 0; i<10; i++) {
+                balls.push(Matter.Bodies.circle(width/2, i*10+30, 6));
+            }
+            Matter.World.add(engine.world, balls);
+
+            const ground = Matter.Bodies.rectangle(width/2, height, width, 10, {isStatic: true});
+            Matter.World.add(engine.world, [ground]);
         });
-
-        // create two boxes and a ground
-        const balls = [];
-
-        for (let i = 0; i<10; i++) {
-            balls.push(Matter.Bodies.circle(this.props.width/2, i*10+30, 6));
-        }
-        Matter.World.add(engine.world, balls);
-
-        const ground = Matter.Bodies.rectangle(this.props.width/2, this.props.height, this.props.width, 10, {isStatic: true});
-        Matter.World.add(engine.world, [ground]);
 
         // run the engine
         Matter.Engine.run(engine);
